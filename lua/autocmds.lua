60c3484cad98615d3aa9f725c12e0cce89a85dd4
itztla <hanumanman37@gmail.com>
2025-09-04 09:46:26 +0700

    autocmds: setup oil.code
    this exists????

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -7,3 +7,38 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank({ timeout = 50 })
 	end,
 })
+
+local vscode = require("vscode")
+local map = vim.keymap.set
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*" },
+	callback = function()
+		map("n", "-", function()
+			vscode.action("oil-code.open")
+		end)
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "oil" },
+	callback = function()
+		map("n", "-", function()
+			vscode.action("oil-code.openParent")
+		end)
+		map("n", "_", function()
+			vscode.action("oil-code.openCwd")
+		end)
+		map("n", "<CR>", function()
+			vscode.action("oil-code.select")
+		end)
+		map("n", "<C-t>", function()
+			vscode.action("oil-code.selectTab")
+		end)
+		map("n", "<C-l>", function()
+			vscode.action("oil-code.refresh")
+		end)
+		map("n", "`", function()
+			vscode.action("oil-code.cd")
+		end)
+	end,
+})
