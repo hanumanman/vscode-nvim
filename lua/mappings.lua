0e6dddd721a142b4a886f36324fc726291009e4e
itztla <hanumanman37@gmail.com>
2025-09-14 16:32:35 +0700

    keymaps: Add descriptions to key mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,23 +10,27 @@ map("n", "die", "ggdG", { desc = "Change all" })
 -- Utils
 map("n", "<leader>f", function()
 	vscode.action("workbench.action.files.save")
-end)
+end, { desc = "Save file" })
 
 map("n", "<leader>s", function()
 	vscode.action("workbench.action.quickOpen")
-end)
+end, { desc = "Search files" })
 
 map({ "n", "v" }, "<leader>a", function()
 	vscode.action("workbench.action.showCommands")
-end)
+end, { desc = "Show commands palette" })
 
 map("n", "<leader>z", function()
 	vscode.action("workbench.action.toggleZenMode")
-end)
+end, { desc = "Toggle Zen Mode" })
 
 map({ "v", "n" }, "<leader>ii", function()
 	vscode.action("inlineChat.start")
-end)
+end, { desc = "Start Inline Chat" })
+
+map({ "v", "n" }, "<leader>ij", function()
+	vscode.action("workbench.panel.chat.view.copilot.focus")
+end, { desc = "Focus Copilot Chat" })
 
 -- Git
 map({ "v", "n" }, "<leader>hr", function()
@@ -40,97 +44,89 @@ map({ "v", "n" }, "<leader>hr", function()
 			end
 		end,
 	})
-end)
+end, { desc = "Revert selected changes" })
 
 map("n", "<leader>hs", function()
 	vscode.action("git.stageSelectedRanges")
-end)
+end, { desc = "Stage selected changes" })
 
 map("n", "<leader>ha", function()
 	vscode.action("git.stageAll")
-end)
+end, { desc = "Stage all changes" })
 
 map("n", "]c", function()
 	vscode.action("workbench.action.editor.nextChange")
 	vscode.action("workbench.action.compareEditor.nextChange")
-end)
+end, { desc = "Go to next change" })
 
 map("n", "[c", function()
 	vscode.action("workbench.action.editor.previousChange")
-end)
+end, { desc = "Go to previous change" })
 
 -- Lsp
 map("n", "gd", function()
 	vscode.action("editor.action.revealDefinition")
-end)
+end, { desc = "Go to definition" })
 
 map("n", "gr", function()
 	vscode.action("editor.action.goToReferences")
-end)
+end, { desc = "Go to references" })
 
 map("n", "gt", function()
 	vscode.action("editor.action.peekTypeDefinition")
-end)
+end, { desc = "Peek type definition" })
 
 map({ "n", "v" }, "<leader>ca", function()
 	vscode.action("editor.action.quickFix")
-end)
+end, { desc = "Show code actions" })
 
 map("n", "<leader>rn", function()
 	vscode.action("editor.action.rename")
-end)
+end, { desc = "LSP Rename" })
 
 map("n", "]d", function()
 	vscode.action("editor.action.marker.next")
-end)
+end, { desc = "Go to next diagnostic" })
 
 map("n", "[d", function()
 	vscode.action("editor.action.marker.prev")
-end)
+end, { desc = "Go to previous diagnostic" })
 
 map("n", "<leader>ds", function()
 	vscode.action("workbench.action.gotoSymbol")
-end)
+end, { desc = "Search LSP symbols" })
 
 map("n", "<leader>dw", function()
 	vscode.action("search.action.openNewEditor")
-end)
+end, { desc = "Grep in workspace" })
 
 -- File explorer
-map("n", ",", function()
+map("n", "<leader>e", function()
 	vscode.action("workbench.view.explorer")
-end)
+end, { desc = "Open file explorer" })
 
 -- Terminal
 map("n", "<C-j>", function()
 	vscode.action("workbench.action.terminal.toggleTerminal")
-end)
+end, { desc = "Toggle terminal" })
 
 -- Tab controls
 map("n", "L", function()
 	vscode.action("workbench.action.nextEditorInGroup")
-end)
+end, { desc = "Next tab" })
 
 map("n", "H", function()
 	vscode.action("workbench.action.previousEditorInGroup")
-end)
-
-map("n", "<C-H>", function()
-	vscode.action("workbench.action.moveEditorLeftInGroup")
-end)
-
-map("n", "<C-L>", function()
-	vscode.action("workbench.action.moveEditorRightInGroup")
-end)
+end, { desc = "Previous tab" })
 
 map("n", "<leader>db", function()
 	vscode.action("workbench.action.showAllEditors")
-end)
+end, { desc = "List open buffers" })
 
 map("n", "<leader>bc", function()
 	vscode.action("workbench.action.closeOtherEditors")
-end)
+end, { desc = "Close other buffers" })
 
 map("n", "<leader>x", function()
 	vscode.action("workbench.action.closeActiveEditor")
-end)
+end, { desc = "Close current buffer" })
